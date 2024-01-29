17c5feb54f07b2aea0d7fd3db3c2dd0c49ebe284
canno-t <janpekalski939@gmail.com>
2024-01-29 09:17:12 +0100

    feat(007) response logic moved to builder

--- app/Responses/ResponseBuilder.php
+++ app/Responses/ResponseBuilder.php
@@ -15,7 +15,7 @@ class ResponseBuilder implements AppResponse
         $this->status = $status;
     }
 
-    public static function setResponse($status, $message){
+    public static function setResponse($status, $message=null){
         $status = ($status === false) ? 'fail' : 'success';
         $message  = $message?:static::$succesResponse;
         return new self($status, $message);
