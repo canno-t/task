3520eecda30e4fd854b8b6c66efad8c37724c1db
canno-t <janpekalski939@gmail.com>
2024-04-01 15:06:18 +0200

    feat(011v1) fix parent task id

--- app/Entities/Task/TaskEntity.php
+++ app/Entities/Task/TaskEntity.php
@@ -2,10 +2,20 @@
 
 namespace App\Entities\Task;
 
+use App\Models\Task;
+
 class TaskEntity
 {
     public function fromArray(array $args){
-        return '1' === $args['tasktype'] ? SubTaskEntity::fromArray($args) : MainTaskEntity::fromArray($args);
+        return '2' === $args['tasktype'] ? SubTaskEntity::fromArray($args) : MainTaskEntity::fromArray($args);
+    }
+
+    public function fromExistingTask(Task $task){
+        if($task->task_type_id === '1'){
+            return array_values($task->only('name', '$description', 'author_id', 'finish_date'));
+//            return MainTaskEntity::fromArray()
+        }
+        return array_values($task->only('name', 'description', 'author_id', 'finish_date', 'dominant_task_id'));
     }
 
 }
