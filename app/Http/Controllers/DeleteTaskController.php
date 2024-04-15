25e301c832bead9366fba441bce0cc2a88bd856e
canno-t <janpekalski939@gmail.com>
2024-04-15 09:51:49 +0200

    feat(017) void service

--- app/Http/Controllers/DeleteTaskController.php
+++ app/Http/Controllers/DeleteTaskController.php
@@ -25,9 +25,6 @@ class DeleteTaskController extends Controller
         if ($request->user()->cannot('delete', $task)) {
             return response()->json(TaskResponse::setResponse(false, 'You are not the author of this task')->returnResponse());
         }
-        if ($this->deleteTaskService->delete($task)) {
-            return response()->json(TaskResponse::setResponse(true, 'task deleted successfully')->returnResponse());
-        }
-        return response()->json(TaskResponse::setResponse(false, 'cannot delete task')->returnResponse());
+        return response()->json(TaskResponse::setResponse(true, 'task deleted successfully')->returnResponse());
     }
 }
--- app/Services/DeleteTaskService.php
+++ app/Services/DeleteTaskService.php
@@ -2,20 +2,20 @@
 
 namespace App\Services;
 
+use App\Exceptions\DatabaseException;
 use App\Models\Task;
 use App\Models\Assignments;
 
 class DeleteTaskService
 {
-    public function delete(Task $task):bool{
+    public function delete(Task $task):void{
         try{
             foreach (Assignments::where('task_id', $task['id'])->get() as $item) {
                 $item->delete();
             }
             $task->delete();
         }catch (\Exception $e){
-            return 0;
+            throw new DatabaseException();
         }
-        return 1;
     }
 }
